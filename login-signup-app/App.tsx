e32e3cee72d582615011d8e3f3465493845818e9
freercurse <freerjoseph@yahoo.co.yk>
2022-05-22 22:41:56 +0100

    updated env and organsed files into folders

--- login-signup-app/App.tsx
+++ login-signup-app/App.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-
+import 'dotenv/config'
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
@@ -8,22 +8,21 @@ import { getDatabase } from "firebase/database";
 import { initializeApp } from 'firebase/app';
 import { Bytes, getFirestore } from "firebase/firestore";
 import { LogBox } from 'react-native';
-
+require('dotenv').config()
 
 import { useState } from 'react';
 
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCtaianGPpG03qcyE3YPY5oay3SvXkq8P4",
-  authDomain: "quabble-7fced.firebaseapp.com",
-  projectId: "quabble-7fced",
-  storageBucket: "quabble-7fced.appspot.com",
-  messagingSenderId: "86011789417",
-  appId: "1:86011789417:web:a4d14934164165fb5922f8",
-  measurementId: "G-BL9KXEWCBV",
-  databaseURL: 'https://quabble-7fced-default-rtdb.europe-west1.firebasedatabase.app/'
-
+  apiKey: process.env.API,
+  authDomain: process.env.AUTH,
+  projectId: process.env.PROJECT,
+  storageBucket: process.env.STORAGE,
+  messagingSenderId: process.env.MESSAGE,
+  appId: process.env.APP,
+  measurementId: process.env.MESUREMENT,
+  databaseURL: process.env.DATABASE
 };
-
 
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
